51d8e7fe5d2ab707b517acc73466b07d3f266663
Biswajit Biswas <jfbiswajit@gmail.com>
2024-05-14 13:38:21 +0600

    doc: Documentation added

---
+++ README.md
@@ -0,0 +1,25 @@
+# TranslaFlex
+
+#### composer.json
+
+```
+"repositories": [
+    {
+        "type": "vcs",
+        "url": "https://github.com/jfBiswajit/translaflex"
+    }
+]
+
+```
+
+#### Install
+
+`composer require jfBiswajit/translaflex`
+
+#### Publish
+
+`php artisan laraflex:publish`
+
+#### Migrate
+
+`php artisan migrate`
--- src/TranslaFlexServiceProvider.php
+++ src/TranslaFlexServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use JfBiswajit\TranslaFlex\Console\Command\PublishTranslaFlex;
 use JfBiswajit\TranslaFlex\Livewire\Translations\CreateTranslationComponent;
 use Livewire\Livewire;
+use Illuminate\Support\Facades\App;
+
 
 class TranslaFlexServiceProvider extends ServiceProvider
 {
@@ -15,12 +17,14 @@ class TranslaFlexServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'translaflex');
-        Livewire::component('tanslaflex::create-translation', CreateTranslationComponent::class);
+        if (App::isLocal()) {
+            $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+            Livewire::component('tanslaflex::create-translation', CreateTranslationComponent::class);
+            $this->loadViewsFrom(__DIR__ . '/../resources/views', 'translaflex');
 
-        if ($this->app->runningInConsole()) {
-            $this->commands(PublishTranslaFlex::class);
+            if ($this->app->runningInConsole()) {
+                $this->commands(PublishTranslaFlex::class);
+            }
         }
     }
 }
